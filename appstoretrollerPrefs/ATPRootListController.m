c46b0059c0fffedd8e56c8d82079efe701c750ba
verygenericname <dedbeddedbed@gmail.com>
2025-04-08 17:18:42 -0400

    cursed

--- appstoretrollerPrefs/ATPRootListController.m
+++ appstoretrollerPrefs/ATPRootListController.m
@@ -19,20 +19,55 @@
 }
 
 - (void)respring {
-    NSUserDefaults *defaults = [[NSUserDefaults alloc] initWithSuiteName:@"dev.mineek.appstoretroller"];
+    for (UIWindow *window in [UIApplication sharedApplication].windows) {
+        if (window.isKeyWindow) {
+            [window endEditing:YES];
+            break;
+        }
+    }
     
+    if (access(THEOS_PACKAGE_INSTALL_PREFIX "/var/mobile/Library/Preferences/dev.mineek.appstoretroller.plist", F_OK)) {
+        NSString *prefsPath = @THEOS_PACKAGE_INSTALL_PREFIX "/var/mobile/Library/Preferences/dev.mineek.appstoretroller.plist";
+        NSDictionary *placeholder = @{};
+
+        NSData *data = [NSPropertyListSerialization dataWithPropertyList:placeholder
+                                                                format:NSPropertyListBinaryFormat_v1_0
+                                                               options:0
+                                                                 error:nil];
+        [data writeToFile:prefsPath atomically:YES];
+    }
+        
+    NSUserDefaults *defaults = [[NSUserDefaults alloc] initWithSuiteName:@"dev.mineek.appstoretroller"];
+        
     NSString *prefsPath = @THEOS_PACKAGE_INSTALL_PREFIX "/var/mobile/Library/Preferences/dev.mineek.appstoretroller.plist";
     BOOL enabled = [defaults boolForKey:@"enabled"];
     BOOL updatesEnabled = [defaults boolForKey:@"updatesEnabled"];
     NSString *iOSVersion = [defaults stringForKey:@"iOSVersion"];
     
-    NSDictionary *prefs = @{
+    if (!iOSVersion) {
+        [defaults setBool:NO forKey:@"enabled"];
+        enabled = NO;
+    }
+    
+    if (updatesEnabled == NO) {
+        [defaults setBool:NO forKey:@"updatesEnabled"];
+    }
+    
+    NSMutableDictionary *prefs = [NSMutableDictionary dictionaryWithDictionary:@{
         @"enabled": @(enabled),
-        @"updatesEnabled": @(updatesEnabled),
-        @"iOSVersion": iOSVersion
-    };
-
-    [prefs writeToFile:prefsPath atomically:YES];
+        @"updatesEnabled": @(updatesEnabled)
+    }];
+    
+    if (iOSVersion) {
+        prefs[@"iOSVersion"] = iOSVersion;
+    }
+        
+    NSData *plistData = [NSPropertyListSerialization dataWithPropertyList:prefs
+                                                                format:NSPropertyListBinaryFormat_v1_0
+                                                               options:0
+                                                                 error:nil];
+    
+    [plistData writeToFile:prefsPath atomically:YES];
     
     NSTask *t = [[NSTask alloc] init];
     [t setLaunchPath:@THEOS_PACKAGE_INSTALL_PREFIX "/usr/local/bin/appstoretrollerKiller"];

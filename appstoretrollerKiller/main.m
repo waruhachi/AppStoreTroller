84e3acfc2a801e536db8a914945489d6431fed4a
verygenericname <dedbeddedbed@gmail.com>
2025-04-16 19:30:35 -0400

    fix killing processes sometimes

--- appstoretrollerKiller/main.m
+++ appstoretrollerKiller/main.m
@@ -3,19 +3,14 @@
 #import <rootless.h>
 #import "TSUtil.h"
 
-@interface NSTask : NSObject
-@property (copy) NSArray *arguments;
-@property (copy) NSString *launchPath;
-- (id)init;
-- (void)waitUntilExit;
-- (void)launch;
-@end
-
-
 int main(int argc, char *argv[], char *envp[]) {
 	@autoreleasepool {
         if (getuid() == 501) {
-            spawnRoot(ROOT_PATH_NS(@"/usr/local/bin/appstoretrollerKiller"), nil, nil, nil);
+            if (argc > 1 && strcmp(argv[1], "--child") == 0) {
+                exit(1);
+            }
+            
+            spawnRoot(ROOT_PATH_NS(@"/usr/local/bin/appstoretrollerKiller"), @[ @"", @"--child" ], nil, nil);
             exit(0);
         }
         killall(@"appstored", NO);
